1314665f72ba925d5f2795d95d3ff8db0423d1ae
John Schmid <jschmid808@gmail.com>
2017-07-10 21:34:55 -0400

    Adding linking to views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,13 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   # GET /articles
   # GET /articles.json
   def index
     @articles = Article.all
+  end
+
+  # articles/all
+  def all
   end
 
   # GET /articles/1
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 	resources :articles
 	root 'articles#index'
 
+	get 'articles/all' => 'articles#all'
+
 	# The priority is based upon order of creation: first created -> highest priority.
 	# See how all your routes lay out with "rake routes".
 
